8d1bb82812a97fba7bddb0998f9b64a129e7a2f9
Oksanaka848 <okmitka@mail.ru>
2023-02-19 22:33:50 +0300

    ch

--- langus/src/components/WordList/WordList.jsx
+++ langus/src/components/WordList/WordList.jsx
@@ -5,24 +5,18 @@ import style from './style.module.scss';
 export default function WordList() {
   return (
     <><div className={style.container}>
-    <div className={style.listcontainer}>
-    <div type="text" className={style.item}> Слово</div>
-    <div type="text" className={style.item}>Перевод</div>
-    <div type="text" className={style.item}>Транскрипция</div> 
+      <div className={style.listcontainer}>
+        <div type="text" className={style.item}> Слово</div>
+        <div type="text" className={style.item}>Перевод</div>
+        <div type="text" className={style.item}>Транскрипция</div>
         {
-      words.map((word) =>
-        <Words key={word.id} en={word.en} ru={word.ru} tr={word.tr}>
-        </Words>
-      )
-    }
-      
-   
-
-        
-        </div >
-        
-</div>
-
-</>
+          words.map((word) =>
+            <Words key={word.id} en={word.en} ru={word.ru} tr={word.tr}>
+            </Words>
+          )
+        }
+      </div >
+    </div>
+    </>
   )
 }

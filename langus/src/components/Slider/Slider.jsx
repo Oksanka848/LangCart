e455eec7d917556fe1ca7e0f7cd8435d52e5bd32
Oksanaka848 <okmitka@mail.ru>
2023-02-26 22:38:18 +0300

    add Slider

--- langus/src/components/Slider/Slider.jsx
+++ langus/src/components/Slider/Slider.jsx
@@ -8,6 +8,52 @@ export default function Slider() {
 const [count, setCount] = useState (0);
 
 function handlePrev(){
+  if(count !== setCount){
+    setCount(count - 1)
+  } 
+  else if (count === Slides.length){
+    setCount(count + 1)
+  }
+}
+
+function handleNext(){
+  if(count !== setCount){
+    setCount(count + 1)
+  }
+  else if (count === Slides.length){
+    setCount(Slides.length)
+  }
+}
+
+return (
+  <>
+    <div className={style.container_slider}>
+       <button onClick={() => handlePrev()}  >&#8592;</button>
+        <Slides key={words[count]} words = {words[count] || {en:'', tr:'', ru:'', id:''}}
+     
+    />        
+    <button onClick={() => handleNext()} >&#8594;</button> 
+            
+                </div>
+                                </>
+                  
+)
+
+}
+
+/*useEffect(() => {
+     setInterval(() => {
+          setCount((current) => {
+                   const res = current === Slides.length - 1 ? 0 : current + 1
+                  return res
+      })
+  }, 3000)
+  return () => Slides.length + 1
+}, [])
+*/
+/*const prevImgIndex = count ? count - 1 : Slides.length - 1
+const nextImgIndex  = count === Slides.length - 1 ? 0 : count + 1*/
+/*function handlePrev(){
   setCount(count - 1);
 
     
@@ -15,26 +61,6 @@ function handlePrev(){
 function handleNext(){
   setCount(count + 1)
 };
-
-
-
-return (
-  <>
-    <div className={style.container_slider}>
-    <button onClick={handlePrev}  >&#8592;</button>
-    
-    <Slides words = {words[count]}
-    
-    />        
-    <button onClick={handleNext} >&#8594;</button> 
-              
-               
-            
-                </div>
-                
-                </>
-                  
-)
-
-}
-
+useEffect(() => console.log(count), [
+  count,
+]);*/
--- langus/src/components/Slider/Slides.jsx
+++ langus/src/components/Slider/Slides.jsx
@@ -12,13 +12,60 @@ export default function Slides ({words}) {
         <div className={style.item} >{en}</div>
         <div className={style.item} >{tr}</div>
         <div className={style.item} >{ru}</div>
-        <div className={style.item} >{id}</div>
+        <div className={style.index}>{id}</div>
       </div>
+      
     </div>
   )
 }
-/*{click ? (
+/*const [activeIndex, setActiveIndex] = useState(0);
+ 
+// Хук Effect
+useEffect(() => {
+    // Запускаем интервал
+    const interval = setInterval(() => {
+        // Меняем состояние
+        setActiveIndex((current) => {
+            // Вычисляем индекс следующего слайда, который должен вывестись
+            const res = current === img.length - 1 ? 0 : current + 1
+            // Возвращаем индекс
+            return res
+        })
+    }, 3000)
+    // Выключаем интервал
+    return () => clearInterval()
+}, [])
+ 
+// Вычисляем индекс предыдущего слайда
+const prevImgIndex = activeIndex ? activeIndex - 1 : img.length - 1
+// Вычисляем индекс следующего слайда
+const nextImgIndex = activeIndex === img.length - 1 ? 0 : activeIndex + 1
+<div className={style.container_slider}>
+    {words.children}
+    <button onClick={prevImgIndex}  >&#8592;</button>
+    
+    <Slides key={words[count]} words = {words[count]}
+    className={count === words[count] + 1 ? " active" : " "}
+    />        
+    <button onClick={nextImgIndex} >&#8594;</button> 
+              
+               
+            
+                </div>
+
+
+
+
+{click ? (
           <div className={style.item} onClick={handleClick} >{ru}</div>
         ) : (
           <button className={style.button_tr} onClick={handleClick} >Перевод</button>
         )}*/
+        /*function handleNext(){
+          if(count !== 0){
+            setCount(count - 1)
+          }
+          else if (count === 0){
+            setCount(Slides.length)
+          }
+        }*/

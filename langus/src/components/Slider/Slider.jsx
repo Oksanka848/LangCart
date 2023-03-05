af172b944e8ae14ad7f53941c4c8256eb7aa65ea
Oksanaka848 <okmitka@mail.ru>
2023-03-05 19:25:24 +0300

    add idex count

--- langus/src/components/Slider/Slider.jsx
+++ langus/src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import words from '../diction.json';
 import Slides from './Slides';
 import style from './style.module.scss';
@@ -6,52 +6,35 @@ import style from './style.module.scss';
 export default function Slider() {
   const [count, setCount] = useState(0);
   function handlePrev() {
-    if (count !== setCount) {
-      setCount(count - 1)
-    }
-    else if (count === Slides.length) {
-      setCount(count + 1)
+    setCount(count - 1);
+    if (count === 0) {
+      setCount(words.length - 1);
     }
   }
   function handleNext() {
-    if (count !== setCount) {
-      setCount(count + 1)
-    }
-    else if (count === Slides.length) {
-      setCount(Slides.length)
+    setCount(count + 1);
+    if (count === words.length - 1) {
+      setCount(0);
     }
   }
+  const cardIndex = () => {
+
+    if (count !== words.length) {
+      setCount(count + 1);
+    }
+  };
   return (
     <>
-      <div className={style.container_slider}>
-        <button onClick={() => handlePrev()}  >&#8592;</button>
-        <Slides key={words[count]} words={words[count] || { en: '', tr: '', ru: '', id: '' }}/>
-        <button onClick={() => handleNext()} >&#8594;</button>
+      <div className={style.container_wrap}>
+        <div className={style.container_slider}>
+          <button onClick={() => handlePrev()}  >&#8592;</button>
+          <Slides key={words[count]} words={words[count]} cardIndex={cardIndex} />
+          <button onClick={() => handleNext()} >&#8594;</button>
+        </div>
+        <div>
+          {count + 1} / {words.length}
+        </div>
       </div>
     </>
   )
 }
-
-/*useEffect(() => {
-     setInterval(() => {
-          setCount((current) => {
-                   const res = current === Slides.length - 1 ? 0 : current + 1
-                  return res
-      })
-  }, 3000)
-  return () => Slides.length + 1
-}, [])
-*/
-/*const prevImgIndex = count ? count - 1 : Slides.length - 1
-const nextImgIndex  = count === Slides.length - 1 ? 0 : count + 1*/
-/*function handlePrev(){
-  setCount(count - 1);
-
-    
-};
-function handleNext(){
-  setCount(count + 1)
-};
-useEffect(() => console.log(count), [
-  count,
-]);*/

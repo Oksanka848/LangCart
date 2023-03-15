8c7b66c3cbbd91f7bab5e8f4f896cf5d931f0d66
Oksanaka848 <okmitka@mail.ru>
2023-03-15 21:55:06 +0300

    add rout

--- langus/src/components/Header/Header.jsx
+++ langus/src/components/Header/Header.jsx
@@ -1,14 +1,69 @@
 import React from 'react';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import AddWord from '../AddWord/AddWord';
+import Cards from '../Cards/Cards';
 import style from './style.module.scss';
-
+import WordList from '../WordList/WordList';
+import Slider from '../Slider/Slider';
 
 export default function Header() {
   return (
     <>
-    <div className={style.hero_wrapp}>
-    <div className={style._section}></div>
-      </div>
+    
+    
+    <Router>
+    
+    <div className={style.header_container}>
+    <div className={style.header_wrapp}>
+      
+        <nav>
+          <ul>
+            <li>
+              <Link to="/WordList">WordList</Link>
+            </li>
+            <li>
+              <Link to="/Cards">Cards</Link>
+            </li>
+            <li>
+              <Link to="/AddWord">AddWord</Link>
+            </li>
+            <li>
+              <Link to="/Slider">Slider</Link>
+            </li>
+          </ul>
+        </nav>
+        </div>
+        </div>
+      
+      
+        <Routes>
+          <Route path="/Cards" element={<Cards />} />
+          <Route path="/WordList" element={<WordList />} />
+          <Route path="/AddWord" element={<AddWord />} />
+          <Route path="/Slider" element={<Slider />} />
+        </Routes>
+      
+    </Router>
+    
+      
     </>
-  )
+  );
 }
+
+/*function Home() {
+  return <h2>Home</h2>;
+}*/
+
+/*function Cards() {
+  return <h2>Cards</h2>;
+}*/
+
+/*function Users() {
+  return <h2>Users</h2>;
+}
+*/

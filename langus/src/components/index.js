b796b80fef043c41111c36f8f6c91dd0207c7c2c
Oksanaka848 <okmitka@mail.ru>
2023-03-18 20:35:08 +0300

    add index file to pages folder

---
+++ langus/src/components/index.js
@@ -0,0 +1,5 @@
+import WordList from '../pages/WordList/WordList';
+import AddWordList from '../pages/AddWord/AddWordList';
+import Slider from '../pages/Slider/Slider';
+
+export {WordList, AddWordList, Slider}

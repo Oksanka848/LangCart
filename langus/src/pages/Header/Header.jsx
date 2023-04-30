5f29df274abc8eae8e31dd8bacbb1b597ded926a
Oksanaka848 <okmitka@mail.ru>
2023-04-30 22:00:57 +0300

    added link to home page

--- langus/src/pages/Header/Header.jsx
+++ langus/src/pages/Header/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
             <nav>
               <ul>
                 <li>
-                  <CustomLink to="/" end><img src={require('./mylogo.svg')} alt="Home" className={style.logo}/></CustomLink>
+                  <CustomLink to="/Words" end><img src={require('./mylogo.svg')} alt="Home" className={style.logo}/></CustomLink>
                 </li>
                 <li>
                   <CustomLink to="/Cards" end>Cards</CustomLink>

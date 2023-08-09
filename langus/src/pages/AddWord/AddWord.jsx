b6657350c5a6deb01b8beaf66c4fc631e4a51d1a
Oksanaka848 <okmitka@mail.ru>
2023-08-09 20:55:44 +0300

    ch

--- langus/src/pages/AddWord/AddWord.jsx
+++ langus/src/pages/AddWord/AddWord.jsx
@@ -4,9 +4,9 @@ import Words from '../../components/Words/Words';
 
 export default function AddWord(props) {
     const { en, ru, tr } = props;
-    const [words, setWords] = useState(props);
+    //const [words, setWords] = useState(props);
     const [isEditMode, setIsEditMode] = useState(props);
-    const [value, setValue] = useState('');
+    //const [value, setValue] = useState('');
 
     const handleEditMode = (props, e) => { //редактирование поля
         e = e || window.event;
@@ -16,35 +16,53 @@ export default function AddWord(props) {
     /*const add = () => { // добавление элемента
         setWords([...words]);
      }*/
-     const handleSave = () => { // завершение редактирования
-        setIsEditMode(!isEditMode);
-    };
-     const changeValue = (e) => { // изменение содержания поля input
+     /*const handleSave = () => { // завершение редактирования
+        setIsEditMode(isEditMode);
+    };*/
+     /*const changeValue = (e) => { // изменение содержания поля input
         setIsEditMode(e.target.value);
-     }
+     }*/
     const handleCancel = () => { // завершение редактирования
         setIsEditMode(!isEditMode);
     };
-    //const remove = (event) => {setWords([...props(0, index), ...props(index + 1)]);}
+    //const handleCancel = (e) => {setIsEditMode([...props, ...{[props]: e.target.value}]);}
     return (
         <div className={style.listwrap}>
             <div className={style.listcontainer}>
                 {isEditMode ? (
-                    <> <input className={style.input} value={isEditMode.en} onChange={(e) => handleEditMode(en, e)} />
-                        <input className={style.input} value={isEditMode.ru} onChange={(e) => handleEditMode(ru, e)} />
-                        <input className={style.input} value={isEditMode.tr} onChange={() => handleEditMode(ru, tr)} />
+                    <> <input className={style.input} value={isEditMode.en} defaultValue={en} onChange={(e) => setIsEditMode(e.target.value)} />
+                        <input className={style.input} value={isEditMode.ru} defaultValue={ru} onChange={(e) => handleEditMode(e.target.value)} />
+                        <input className={style.input} value={isEditMode.tr} defaultValue={tr} onChange={(e) => handleEditMode(e.target.value)} />
                         <div className={style.buttons}>
-                    <div className={style.button} onClick={handleSave}>Сохранить</div>
+                        <div className={style.button} onClick={handleCancel}>Сохранить</div>
                     <div className={style.button} onClick={handleCancel}>Отмена</div>
                 </div></>
-                ) : (<>
+                ) : (<> 
                     <Words key={props.id} en={props.en} ru={props.ru} tr={props.tr} />
                     <div className={style.buttons}>
                     <div className={style.button} onClick={handleEditMode}>Редактировать</div>
-                    <div className={style.button} onClick={handleCancel}>Удалить</div>
+                    
+                    <div className={style.button} onClick={handleCancel}>Отмена</div>
                 </div></>
                 )
                 }
                             </div ></div>
     )
 }
+/*{isEditMode ? (
+                    <> <input className={style.input} value={isEditMode.en} defaultValue={en} onChange={(e) => handleEditMode(e.target.value)} />
+                        <input className={style.input} value={isEditMode.ru} defaultValue={ru} onChange={(e) => handleEditMode(e.target.value)} />
+                        <input className={style.input} value={isEditMode.tr} defaultValue={tr} onChange={(e) => handleEditMode(e.target.value)} />
+                        <div className={style.buttons}>
+                        <div className={style.button} onClick={handleCancel}>Сохранить</div>
+                    <div className={style.button} onClick={handleCancel}>Отмена</div>
+                </div></>
+                ) : (<>
+                    <Words key={props.id} en={props.en} ru={props.ru} tr={props.tr} />
+                    <div className={style.buttons}>
+                    <div className={style.button} onClick={handleEditMode}>Редактировать</div>
+                    
+                    <div className={style.button} onClick={handleCancel}>Удалить</div>
+                </div></>
+                )
+                }*/

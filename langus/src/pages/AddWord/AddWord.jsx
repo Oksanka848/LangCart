e5e21b3208a868f4034008bdba4204734a743f0a
Oksanaka848 <okmitka@mail.ru>
2023-08-12 16:17:25 +0300

    formatted code

--- langus/src/pages/AddWord/AddWord.jsx
+++ langus/src/pages/AddWord/AddWord.jsx
@@ -1,119 +1,138 @@
-import React, { useState } from 'react'
-import style from './style.module.scss';
+import React, { useState } from "react";
+import style from "./style.module.scss";
 
 export default function AddWord({ rows, actions }) {
-            const [isEditMode, setIsEditMode] = useState(false);
-        const [rowIDToEdit, setRowIDToEdit] = useState(undefined);
-        const [rowsState, setRowsState] = useState(rows);
-        const [editedRow, setEditedRow] = useState();
-              const handleEdit = (rowID) => {
-          setIsEditMode(true);
-          setEditedRow(undefined);
-          setRowIDToEdit(rowID);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [rowIDToEdit, setRowIDToEdit] = useState(undefined);
+  const [rowsState, setRowsState] = useState(rows);
+  const [editedRow, setEditedRow] = useState();
+  const handleEdit = (rowID) => {
+    setIsEditMode(true);
+    setEditedRow(undefined);
+    setRowIDToEdit(rowID);
+  };
+  const handleRemoveRow = (rowID) => {
+    const newData = rowsState.filter((row) => {
+      return row.id !== rowID ? row : null;
+    });
+    setRowsState(newData);
+  };
+  const handleOnChangeField = (e, rowID) => {
+    const { name: fieldName, value } = e.target;
+    setEditedRow({
+      id: rowID,
+      [fieldName]: value,
+    });
+  };
+  const handleCancelEditing = () => {
+    setIsEditMode(false);
+    setEditedRow(undefined);
+  };
+  const handleSaveRowChanges = () => {
+    setTimeout(() => {
+      setIsEditMode(false);
+      const newData = rowsState.map((row) => {
+        if (row.id === editedRow.id) {
+          if (editedRow.en) row.en = editedRow.en;
+          if (editedRow.ru) row.ru = editedRow.ru;
+          if (editedRow.tr) row.tr = editedRow.tr;
         }
-              const handleRemoveRow = (rowID) => {
-          const newData = rowsState.filter(row => {
-            return row.id !== rowID ? row : null
-          });
-                setRowsState(newData);
-        }
-              const handleOnChangeField = (e, rowID) => {
-          const { name: fieldName, value } = e.target;
-                setEditedRow({
-            id: rowID,
-            [fieldName]: value
-          })
-        }
-              const handleCancelEditing = () => {
-          setIsEditMode(false);
-          setEditedRow(undefined);
-        }
-              const handleSaveRowChanges = () => {
-          setTimeout(() => {
-            setIsEditMode(false);
-                  const newData = rowsState.map(row => {
-              if (row.id === editedRow.id) {
-                if (editedRow.en) row.en = editedRow.en;
-                if (editedRow.ru) row.ru = editedRow.ru;
-                if (editedRow.tr) row.tr = editedRow.tr;
-              }
-                    return row;
-            })
-                  setRowsState(newData);
-            setEditedRow(undefined)
-          })
-        }
-      
-        return (
-          <table className={style.listwrap}>
-                      <tbody>
-                        {rowsState.map((row) => {
-              return <tr className={style.listcontainer} key={row.id}>
-                <td>
-                  {row.id}
-                </td>
-                <td>
-                  { isEditMode && rowIDToEdit === row.id
-                    ? <input
+        return row;
+      });
+      setRowsState(newData);
+      setEditedRow(undefined);
+    });
+  };
+
+  return (
+    <div className={style.listwrap}>
+      <div>
+        {rowsState.map((row) => {
+          return (
+            <div className={style.rowcontainer} key={row.id}>
+              <div>{row.id}</div>
+              <div>
+                {isEditMode && rowIDToEdit === row.id ? (
+                  <input
                     className={style.input}
-                      type='text'
-                      defaultValue={editedRow ? editedRow.en : row.en}
-                      id={row.id}
-                      name='en'
-                      onChange={ (e) => handleOnChangeField(e, row.id) }
-                    />
-                    : row.en
-                  }
-                </td>
-                <td>
-                  { isEditMode && rowIDToEdit === row.id
-                    ? <input
+                    type="text"
+                    defaultValue={editedRow ? editedRow.en : row.en}
+                    id={row.id}
+                    name="en"
+                    onChange={(e) => handleOnChangeField(e, row.id)}
+                  />
+                ) : (
+                  row.en
+                )}
+              </div>
+              <div>
+                {isEditMode && rowIDToEdit === row.id ? (
+                  <input
                     className={style.input}
-                      type='text'
-                      defaultValue={editedRow ? editedRow.ru : row.ru}
-                      id={row.id}
-                      name='ru'
-                      onChange={ (e) => handleOnChangeField(e, row.id) }
-                    />
-                    : row.ru
-                  }
-                </td>
-                <td>
-                  { isEditMode && rowIDToEdit === row.id
-                    ? <input 
+                    type="text"
+                    defaultValue={editedRow ? editedRow.ru : row.ru}
+                    id={row.id}
+                    name="ru"
+                    onChange={(e) => handleOnChangeField(e, row.id)}
+                  />
+                ) : (
+                  row.ru
+                )}
+              </div>
+              <div>
+                {isEditMode && rowIDToEdit === row.id ? (
+                  <input
                     className={style.input}
                     defaultValue={editedRow ? editedRow.tr : row.tr}
                     id={row.id}
                     name="tr"
-                    onChange={e => handleOnChangeField(e, row.id)}  
-                    />
-                    : row.tr
-                  }
-                </td>
-                {actions &&
-                <td className={style.listcontainer}>
-                  { isEditMode && rowIDToEdit === row.id
-                    ? <div onClick={ () => handleSaveRowChanges() } className={style.button} disabled={!editedRow}>
+                    onChange={(e) => handleOnChangeField(e, row.id)}
+                  />
+                ) : (
+                  row.tr
+                )}
+              </div>
+
+              {actions && (
+                <div className={style.buttons}>
+                  {isEditMode && rowIDToEdit === row.id ? (
+                    <div
+                      onClick={() => handleSaveRowChanges()}
+                      className={style.button}
+                      disabled={!editedRow}
+                    >
                       Сохранить
                     </div>
-                    : <div  onClick={ () => handleEdit(row.id) } className={style.button}>
+                  ) : (
+                    <div
+                      onClick={() => handleEdit(row.id)}
+                      className={style.button}
+                    >
                       Редактировать
                     </div>
-                  }
-      
-                  { isEditMode && rowIDToEdit === row.id
-                    ? <div onClick={() => handleCancelEditing()} className={style.button}>
+                  )}
+
+                  {isEditMode && rowIDToEdit === row.id ? (
+                    <div
+                      onClick={() => handleCancelEditing()}
+                      className={style.button}
+                    >
                       Отменить
                     </div>
-                    : <div onClick={() => handleRemoveRow(row.id)} className={style.button}>
+                  ) : (
+                    <div
+                      onClick={() => handleRemoveRow(row.id)}
+                      className={style.button}
+                    >
                       Удалить
                     </div>
-                  }
-                </td>
-                }
-              </tr>
-            })}
-            </tbody>
-          </table>
-        );
-      }
+                  )}
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}

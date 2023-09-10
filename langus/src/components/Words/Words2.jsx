f78e46facb30e846fdcd38d85349a95b6aaec99a
Oksanaka848 <okmitka@mail.ru>
2023-09-10 19:28:08 +0300

    add fetch api

--- langus/src/components/Words/Words2.jsx
+++ langus/src/components/Words/Words2.jsx
@@ -1,42 +1,54 @@
-import React, {useState, useEffect} from 'react'
-
+import React, { useState, useEffect } from 'react'
+import Wordslist2 from './Wordslist2';
 import style from './style.module.scss';
 
 export default function Words2() {
-	let [words, setWords] = useState ([ ]);
-  let [isLoading, setisLoading] = useState (false);
-  let [error, setError] = useState (null);
-	
-	  useEffect(() =>{                     
-      setisLoading(true);
-        fetch('api/words')
-            .then((response) => {
-              if (response.ok) {
-              response.json();}
-            else {throw new Error('Something went wrong ...')}
-            })
-            .then((response) => {setWords(response); setisLoading(false);})
-            .catch((error) => {setError(error); setisLoading(false);});
-    }, [ ]); 
-    if (error) {
-      return <p>{error.message}</p>;
+  const [words, setWords] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('api/words')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then(
+        (result) => {
+          setWords(result);
+        })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setisLoading(true);
+      });
+  }, []);
+
+  if (error) {
+    return <p>{error.message}</p>;
   }
 
-  if (isLoading) {
-      return <p>Loading ...</p>;
+  else if (!isLoading) {
+    return <p>Loading ...</p>;
   }
-  
-
+  else {
     return (
-			   
-                 <><div className={style.container}>
-                 <div className={style.listcontainer}>
-                 <ol>
-             {
-                words.map(word => {
-                    return <li key={word.id}>{word.english} - {word.russian}</li>
-                })
-             }
-	       </ol>
-                 </div>
-               </div> </> ) }
+      <><div className={style.container}>
+        <div className={style.listcontainer}>
+          <div type="text" className={style.item}> Слово</div>
+          <div type="text" className={style.item}>Перевод</div>
+          <div type="text" className={style.item}>Транскрипция</div>
+          {
+            words.map(word => {
+              return <Wordslist2 key={word.id} english={word.english} russian={word.russian} transcription={word.transcription} />
+            })
+          }
+        </div >
+      </div></>)
+  }
+}
